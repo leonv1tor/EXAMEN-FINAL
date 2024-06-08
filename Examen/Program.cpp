8441283f7c8cb925446b102cee16fba92ece17d1
leonv1tor <162352872+leonv1tor@users.noreply.github.com>
2024-06-08 18:45:57 -0400

    2do commit

--- Examen/Program.cpp
+++ Examen/Program.cpp
@@ -10,4 +10,20 @@ string obtenerPrimeraPalabra(string str){
     while(0 < str.sixe() && abc.find(str[0]) == string::npos){
         str.erase(0,1);
     }
+    int pos = str.find(' ');
+    if(pos == string::npos){
+        return str;
+    }
+    return str.substr(0,pos);
+
+}
+
+string eliminarPrimeraPalabra(string cadena){
+    string abc="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890-+";
+    while(0 < cadena.size() && cadena.find(cadena[0]) == string::npos){
+        cadena.erase(0,1);
+}
+int pos = cadena.find(' ');
+cadena.erase(0,pos);
+return cadena;
 }
