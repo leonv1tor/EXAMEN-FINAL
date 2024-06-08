ae88e2b90e9a09d5333a3ac7e68262a00ba7b4b2
leonv1tor <162352872+leonv1tor@users.noreply.github.com>
2024-06-08 16:36:12 -0400

    1er commit

--- Examen/Program.cpp
+++ Examen/Program.cpp
@@ -1,4 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- int hola; 
- cout << hola
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+string obtenerPrimeraPalabra(string str){
+    string abc="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPSDFGHJKLZXCVBNM";
+    while(0 < str.sixe() && abc.find(str[0]) == string::npos){
+        str.erase(0,1);
+    }
+}
